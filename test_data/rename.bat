358bd86ff0f292421d1ff007b3495700c7d6bb8d
chiehan1 <chieh.an.hsu@gmail.com>
2016-10-18 17:53:59 +0800

    input pages of C,D and rename these pages with suffix C,D

--- test_data/rename.bat
+++ test_data/rename.bat
@@ -8,48 +8,110 @@ if not exist "%folder%" (
   goto setFolder
 )
 
-set /p EXT=Please Enter filename Extension(i.e.: .jpg or .tif):
+set /p EXT=Please Enter filename Extension(example: .jpg or .tif):
 set /p titleName=Please Enter newName's form, "title" name: 
 set /p volume=Please Enter "Volume" name:
+set /p cdPages=Please key in the page number of C,D(Separate pages by "space", example: 100 200):
 
 choice /C YNC /M "Are you sure rename all files in the %folder%?"
 if errorlevel 3 goto cancel 
 if errorlevel 2 goto cancel
 if errorlevel 1 goto excute
 
-:excute 
+:excute
 setlocal EnableDelayedExpansion
 
-rem Populate the array with existent files in folder
+::make array of file names in the folder
 cd "%folder%"
 set i=0
 for %%a in (*) do (
    set /A i+=1
    set list[!i!]=%%a
 )
-set Filesx=%i%
+set filesN=%i%
 
-rem Display array elements
-set level1=0
-set level2=-1
-for /L %%i in (1,1,%Filesx%) do (
-	set /a s=%%i%%2
-	if !s!==0 (
-		set /a level1+=1
-		set /a num1=%%i-!level1! 
-		if !num1! lss 10 set newName1=00!num1!b
-		if !num1! geq 10 set newName1=0!num1!b
-		if !num1! geq 100 set newName1=!num1!b
-		ren "!list[%%i]!" %titleName%%volume%-!newName1!%EXT%
-	) else (
-		set /a level2+=1
-		set /a num2=%%i-!level2!
-		if !num2! lss 10 set newName2=00!num2!a
-		if !num2! geq 10 set newName2=0!num2!a
-		if !num2! geq 100 set newName2=!num2!a
-		ren "!list[%%i]!" %titleName%%volume%-!newName2!%EXT%
-	)
+::make array of c,d pages
+set /a cdCount=0
+
+for %%a in (%cdPages%) do (
+  set /a cdCount+=1
+  set cdPages[!cdCount!]=%%a
 )
 
-pause
-:cancel
+if not defined cdPages[1] (
+  set cdPage=noPage
+) else (
+  set /a cdPageI=1
+  call set /a cdPage=%%cdPages[!cdPageI!]%%
+)
+
+::make array of new names
+set /a newNameI=0
+set /a countDown=%filesN%
+
+for /l %%a in (1,1,%filesN%) do (
+  if !countDown! leq 0 goto newNamesMaked
+  set /a newNameI+=1
+  if %%a equ !cdPage! (
+    set /a cdPageI+=1
+    if defined cdPages[!cdPageI!] (
+      call set /a cdPage=%%cdPages[!cdPageI!]%%
+    )
+    if %%a lss 10 (
+      set newNames[!newNameI!]=00%%aa
+      set /a newNameI+=1
+      set newNames[!newNameI!]=00%%ab
+      set /a newNameI+=1
+      set newNames[!newNameI!]=00%%ac
+      set /a newNameI+=1
+      set newNames[!newNameI!]=00%%ad
+    )
+    if %%a geq 10 (
+      if %%a lss 100 (
+        set newNames[!newNameI!]=0%%aa
+        set /a newNameI+=1
+        set newNames[!newNameI!]=0%%ab
+        set /a newNameI+=1
+        set newNames[!newNameI!]=0%%ac
+        set /a newNameI+=1
+        set newNames[!newNameI!]=0%%ad
+      )
+    )
+    if %%a geq 100 (
+      set newNames[!newNameI!]=%%aa
+      set /a newNameI+=1
+      set newNames[!newNameI!]=%%ab
+      set /a newNameI+=1
+      set newNames[!newNameI!]=%%ac
+      set /a newNameI+=1
+      set newNames[!newNameI!]=%%ad
+    )
+    set /a countDown-=4
+  ) else (
+    if %%a lss 10 (
+      set newNames[!newNameI!]=00%%aa
+      set /a newNameI+=1
+      set newNames[!newNameI!]=00%%ab
+    ) 
+    if %%a geq 10 (
+      if %%a lss 100 (
+        set newNames[!newNameI!]=0%%aa
+        set /a newNameI+=1
+        set newNames[!newNameI!]=0%%ab
+      )
+    )
+    if %%a geq 100 (
+      set newNames[!newNameI!]=%%aa
+      set /a newNameI+=1
+      set newNames[!newNameI!]=%%ab
+    )
+    set /a countDown-=2
+  )
+)
+:newNamesMaked
+
+::test
+for /l %%a in (1,1,%filesN%) do (
+  set newName=!newNames[%%a]!
+  ren "!list[%%a]!" %titleName%%volume%-!newName!%EXT%
+)
